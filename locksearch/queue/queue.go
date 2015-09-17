bcf0bb16aa913ef27b940ca26854817caf61845f
David Benque <dbenque@gmail.com>
2015-09-17 23:40:40 +0200

    Graph

--- locksearch/queue/queue.go
+++ locksearch/queue/queue.go
@@ -1,13 +1,14 @@
 package queue
 
 import (
-	"fmt"
 	"sort"
 	"strconv"
 	"strings"
 	"sync"
 
 	"maglogparser/locksearch/record"
+
+	tm "github.com/buger/goterm"
 )
 
 type result struct {
@@ -29,13 +30,13 @@ func StatQueue() error {
 
 	var myresults results
 	var wg sync.WaitGroup
-	fmt.Printf("Length:%d\n", len(record.RecordByThread))
 	for id, sl := range record.RecordByThread {
 		wg.Add(1)
 		go func(tid string, rs []*record.Record) {
 
 			defer wg.Done()
 			for _, r := range rs {
+
 				if len(r.Raw) < 131 {
 					continue
 				}
@@ -45,45 +46,68 @@ func StatQueue() error {
 				}
 
 				tokens := strings.Split(txt[1], " ") // 60:130 to limit the scope of search, with reasonable margins
-				if (tokens[1] == "Queue") && (tokens[2] == "command") {
-
+				if ((tokens[1] == "Queue") && (tokens[2] == "command")) ||
+					((tokens[1] == "Dequeue") && (tokens[2] == "and") && (tokens[3] == "execute")) {
 					//2015/08/04 15:19:59.904847 magap302 masterag-11298 MDW INFO <SEI_MAAdminSequence.cpp#223 TID#13> Queue command 0x2aacbf979400 [Command#14015: kSEIBELibLoaded : BE->MAG:Notify the Master Agent that a lib has been loaded (version 1)^@] (from BENT0UCL4VXODY to masterag); Command sequence queue size: 0
 					backToken := strings.Split(r.Raw[len(r.Raw)-40:len(r.Raw)], " ")
 					last := len(backToken) - 1
-					txt1 := strings.Join(backToken[last-4:last], " ")
-					if txt1 == "Command sequence queue size:" {
-
+					txt1 := strings.Join(backToken[last-2:last], " ")
+					if txt1 == "queue size:" {
 						if s, err := strconv.Atoi(backToken[last]); err == nil {
 							myresults.Lock()
 							myresults.res = append(myresults.res, &result{qSize: s, tid: tid, r: r})
 							myresults.Unlock()
-						} else {
-							fmt.Printf("err #%s#\n", r.Raw)
 						}
 					}
-
 				}
-
 			}
-
 		}(id, sl)
 	}
 
 	wg.Wait()
-	fmt.Println("!! %d", len(myresults.res))
 	sort.Sort(record.ByRecordTime(myresults.res))
-	fmt.Println("!! %d", len(myresults.res))
-	//
-	// w := new(tabwriter.Writer)
-	// w.Init(os.Stdout, 1, 0, 2, ' ', 0)
-	// fmt.Fprintln(w, "Tag\tTID\tRaw Log\t")
-	// for _, r := range myresults.res {
-	// 	res, _ := r.(*result)
-	// 	fmt.Fprintf(w, "%s\t%s\t%s\t\n", res.indicator, res.tid, res.r.Raw)
-	//
-	// }
-	// fmt.Fprintln(w)
-	// w.Flush()
+
+	// Build chart
+	chart := tm.NewLineChart(tm.Width()-10, tm.Height()-10)
+
+	data := new(tm.DataTable)
+	data.AddColumn("Seconds")
+	data.AddColumn("QMax")
+	data.AddColumn("QMin")
+
+	type prec struct {
+		max int
+		min int
+	}
+
+	t0 := myresults.res[0].(*result).r.Time
+	tmax := myresults.res[len(myresults.res)-1].(*result).r.Time
+	indexMax := int(tmax.Sub(t0).Seconds())
+
+	m := make(map[int]prec)
+	for _, v := range myresults.res {
+		r := v.(*result)
+		d := int(r.r.Time.Sub(t0).Seconds())
+		p, ok := m[d]
+		if !ok {
+			p = prec{r.qSize, r.qSize}
+		} else {
+			if p.max < r.qSize {
+				p.max = r.qSize
+			}
+			if p.min > r.qSize {
+				p.min = r.qSize
+			}
+		}
+		m[d] = p
+	}
+
+	for t := 0; t <= int(indexMax); t++ {
+		data.AddRow(float64(t), float64(m[t].max), float64(m[t].min))
+	}
+
+	tm.Println(chart.Draw(data))
+	tm.Flush()
 
 	return nil
 }
